50dac241437856b994ff0751d3b5de052b7a090f
Pratyksh Somani <pratyksh444somani@gmail.com>
2026-07-24 22:48:51 +0530

    Cognizant

---
+++ Week-5/studentapp/src/Components/About.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>Welcome to the About page of the Student Management Portal</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ Week-5/studentapp/src/Components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+            <p>Welcome to the Home page of Student Management Portal</p>
+        </div>
+    );
+}
+
+export default Home;
